63444acb44b9604a23ccb4ea746212d32c891e3a
tanvirahmedkhan74 <tanvirahmedkhan74@gmail.com>
2023-02-18 11:10:29 +0600

    Props and PropTypes

---
+++ Tutorial/textutils/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          {props.title}
+        </a>
+        <button
+          className="navbarS-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                {props.aboutText}
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+/** PropTypes are used to define the type of props passed to the component */
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
+
+/** Default props are used when no props are passed to the component */
+Navbar.defaultProps = {title: 'Set title here',
+aboutText: 'Def About'}
---
+++ Tutorial/textutils/src/module2.mjs
@@ -0,0 +1,5 @@
+const name = "Tanvir";
+const age = 25;
+const address = "Dhaka";
+
+export default age;
